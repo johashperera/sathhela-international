3583470bf97c0cafec1c1eaf9b9c9c8247c39763
johash perera <johash.dev@zohomail.com>
2023-08-05 15:55:20 +0530

    add details section

---
+++ src/components/Details/Details.js
@@ -0,0 +1,55 @@
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+
+const Details = () => {
+  return (
+    <section className="bg-bgHint w-full relative">
+      <div className="px-60 py-20">
+        <div className="grid grid-cols-2">
+          <div>
+            <span className="text-textHint font-medium">WELL CRAFTED</span>
+            <h2 className="text-4xl text-white">We Provide Garments</h2>
+            <span className="text-4xl font-semibold text-white">
+              Clothes that make You Stand Out
+            </span>
+            <p className="text-textHint mt-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut
+              eros sagittis, aliquet nibh vel, rutrum nisi. Orci varius natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Nam vulputate ipsum augue
+            </p>
+            <hr className="my-7" />
+            <div className="grid grid-cols-3">
+              <div className="flex items-center gap-3 text-white">
+                <StaticImage src="../../images/factory.png" width={50} />
+                <div>
+                  <span className="font-semibold text-3xl">2</span>
+                  <p>Factories</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3 text-white">
+                <StaticImage src="../../images/equipment.png" width={50} />
+                <div>
+                  <span className="font-semibold text-3xl">75 +</span>
+                  <p>Machines</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3 text-white">
+                <StaticImage src="../../images/employees.png" width={50} />
+                <div>
+                  <span className="font-semibold text-3xl">100 +</span>
+                  <p>Employees</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white absolute p-5 w-fit right-0 -bottom-5 shadow-xl">
+        <StaticImage src="../../images/machinery.webp" width={600} />
+      </div>
+    </section>
+  );
+};
+
+export default Details;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import AboutUs from "../components/AboutUs/AboutUs";
 import Services from "../components/Services/Services";
+import Details from "../components/Details/Details";
 
 const index = () => {
   return (
@@ -11,6 +12,7 @@ const index = () => {
       <Hero />
       <AboutUs />
       <Services />
+      <Details />
     </main>
   );
 };
