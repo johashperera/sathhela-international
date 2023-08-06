2c8361d218e1f16078b6597ccc00d3141887bf7b
johash perera <johash.dev@zohomail.com>
2023-08-07 00:13:43 +0530

    remove scroll reveal

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,50 +5,17 @@ const Hero = () => {
   return (
     <section className="py-32">
       <div className="text-center flex flex-col gap-4 items-center">
-        <p
-          className="text-primary"
-          data-sal="slide-up"
-          data-sal-duration="500"
-          data-sal-delay="400"
-          data-sal-easing="ease"
-        >
-          Where Fashion meets Authenticity
-        </p>
-        <h1
-          className="text-7xl font-medium uppercase"
-          data-sal="slide-up"
-          data-sal-duration="500"
-          data-sal-delay="500"
-          data-sal-easing="ease"
-        >
+        <p className="text-primary">Where Fashion meets Authenticity</p>
+        <h1 className="text-7xl font-medium uppercase">
           Sathhela International
         </h1>
-        <p
-          data-sal="slide-up"
-          data-sal-duration="500"
-          data-sal-delay="500"
-          data-sal-easing="ease"
-        >
-          Let's step into enhance your lifestyle like a royalty
-        </p>
-        <button
-          className="bg-primary text-white py-2 px-4 w-44"
-          data-sal="slide-up"
-          data-sal-duration="500"
-          data-sal-delay="600"
-          data-sal-easing="ease"
-        >
+        <p>Let's step into enhance your lifestyle like a royalty</p>
+        <button className="bg-primary text-white py-2 px-4 w-44">
           Read More
         </button>
       </div>
       <div className="grid grid-cols-3 gap-7 px-40 mt-16">
-        <div
-          className="text-center flex flex-col items-center shadow-md px-8 py-10"
-          data-sal="slide-up"
-          data-sal-duration="500"
-          data-sal-delay="700"
-          data-sal-easing="ease"
-        >
+        <div className="text-center flex flex-col items-center shadow-md px-8 py-10">
           <StaticImage src="../../images/fabric.png" width={50} />
           <span className="text-lg font-medium mt-5">We Design Fabric</span>
           <p className="text-textHint">
@@ -56,13 +23,7 @@ const Hero = () => {
             metus vel odio pellentesque pellentesque
           </p>
         </div>
-        <div
-          className="text-center flex flex-col items-center shadow-md px-8 py-10"
-          data-sal="slide-up"
-          data-sal-duration="500"
-          data-sal-delay="800"
-          data-sal-easing="ease"
-        >
+        <div className="text-center flex flex-col items-center shadow-md px-8 py-10">
           <StaticImage src="../../images/machine.png" width={50} />
           <span className="text-lg font-medium mt-5">
             We have the Good Stuff
@@ -72,13 +33,7 @@ const Hero = () => {
             metus vel odio pellentesque pellentesque
           </p>
         </div>
-        <div
-          className="text-center flex flex-col items-center shadow-md px-8 py-10"
-          data-sal="slide-up"
-          data-sal-duration="500"
-          data-sal-delay="900"
-          data-sal-easing="ease"
-        >
+        <div className="text-center flex flex-col items-center shadow-md px-8 py-10">
           <StaticImage src="../../images/export.png" width={50} />
           <span className="text-lg font-medium mt-5">Always Genuine</span>
           <p className="text-textHint">
