d5a4eaf07dcaf9c638161ca8c250c2e30c64d25d
johash perera <johash.dev@zohomail.com>
2023-08-08 19:16:11 +0530

    update brand logos

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -4,21 +4,15 @@ import React from "react";
 const Brands = () => {
   return (
     <section className="bg-[#F8F8F8] flex justify-center md:block">
-      <div className="grid grid-cols-2 gap-5 md:flex md:justify-center md:items-center md:gap-16 py-8">
+      <div className="grid grid-cols-1 gap-3 md:flex md:justify-center md:items-center md:gap-16 py-8">
         <div>
-          <StaticImage src="../../images/Brands/1.png" width={150} />
+          <StaticImage src="../../images/Brands/caliber.png" width={150} />
         </div>
         <div>
-          <StaticImage src="../../images/Brands/2.png" width={150} />
+          <StaticImage src="../../images/Brands/dilrich.png" width={150} />
         </div>
         <div>
-          <StaticImage src="../../images/Brands/3.png" width={150} />
-        </div>
-        <div>
-          <StaticImage src="../../images/Brands/4.png" width={150} />
-        </div>
-        <div>
-          <StaticImage src="../../images/Brands/2.png" width={150} />
+          <StaticImage src="../../images/Brands/oleena.png" width={150} />
         </div>
       </div>
     </section>
